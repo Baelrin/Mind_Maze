90bdeffde653b25c479a60aa43076fcb7382588d
Baelrin <Baelrin-Revel@yandex.ru>
2024-03-25 12:20:53 +0300

    Fix errors

--- master.py
+++ master.py
@@ -1,39 +1,54 @@
 import random
 
 
+def print_colored(message, color_code):
+    print(f"\033[{color_code}m{message}\033[0m")
+
+
 def get_user_input(prompt, min_value=1):
     while True:
         try:
             value = int(input(prompt))
             if value < min_value:
-                print(f"Please type a number larger than {min_value} next time.")
+                print_colored("Please type a number larger than 0 next time.", 31)
                 continue
             return value
         except ValueError:
-            print("Please type a number next time.")
+            print_colored("Please type a number next time.", 31)
+
+
+def select_difficulty():
+    print("Select difficulty level:")
+    print("1. Easy")
+    print("2. Medium")
+    print("3. Hard")
+    return get_user_input("Your choice: ")
 
 
 def main():
-    top_of_range = get_user_input("Type a number: ")
+    difficulty = select_difficulty()
+    if difficulty == 1:
+        top_of_range = 10
+    elif difficulty == 2:
+        top_of_range = 100
+    else:
+        top_of_range = 1000
+
     random_number = random.randint(1, top_of_range)
     guesses = 0
     game_over = False
-
-    correct_guess_message = "You got it!"
-    above_message = "Nope! You were above the number!"
-    below_message = "Nope! You were below the number!"
 
     while not game_over:
         guesses += 1
         user_guess = get_user_input("Make a guess: ")
 
         if user_guess == random_number:
-            print(correct_guess_message)
+            print_colored("You got it!", 32)
             game_over = True
         elif user_guess > random_number:
-            print(above_message)
+            print_colored("Nope! You were above the number!", 33)
         else:
-            print(below_message)
+            print_colored("Nope! You were below the number!", 33)
 
     print(f"You got it in {guesses} guesses")
 
